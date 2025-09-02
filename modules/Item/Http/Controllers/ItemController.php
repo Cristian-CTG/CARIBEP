3a3e61870ef033887053e43dbeb054ee6952f799
jeremyabel710 <jeremyabel710@gmail.com>
2025-09-02 16:25:25 -0500

    Issue|#332|Cambio de formato en codigo de barras del producto

--- modules/Item/Http/Controllers/ItemController.php
+++ modules/Item/Http/Controllers/ItemController.php
@@ -12,29 +12,148 @@ use Modules\Item\Imports\ItemListPriceImport;
 use Maatwebsite\Excel\Excel;
 use Modules\Item\Exports\ItemExport;
 use Carbon\Carbon;
+use Intervention\Image\Facades\Image;
 
 class ItemController extends Controller
 {
 
     public function generateBarcode($id)
     {
-
         $item = Item::findOrFail($id);
 
-        $colour = [150,150,150];
+        $company = \App\Models\Tenant\Company::active();
+        $companyName = $company ? $company->name : 'EMPRESA';
+
+        $image = $this->buildBarcodeImage($item, $companyName);
+
+        ob_clean();
+        header('Content-Type: image/png');
+        header('Content-Disposition: attachment; filename="barcode_'.$item->internal_id.'.png"');
+        imagepng($image);
+        imagedestroy($image);
+        exit;
+    }
+    
+    private function buildBarcodeImage($item, $companyName)
+    {
+        $fontPath = public_path('fonts/LiberationSans-Regular.ttf');
+        $fontSize = 14;
+        $fontSizeTitle = 21;
+        $fontSizeCode = 16;
+        $fontSizePrice = 16;
+        $padding = 20;
+        $lineSpacing = 16;
+
+        $title = $companyName;
+        $mainLine = "{$item->name}";
+        if ($item->category && $item->category->name) {
+            $mainLine .= " | {$item->category->name}";
+        }
+        if ($item->brand && $item->brand->name) {
+            $mainLine .= " | {$item->brand->name}";
+        }
+
+        $secondLine = '';
+        if ($item->color && $item->color->name) {
+            $secondLine .= "{$item->color->name}";
+        }
+        if ($item->size && $item->size->name) {
+            $secondLine .= ($secondLine ? " | " : "") . "{$item->size->name}";
+        }
+
+        $bboxTitle = imagettfbbox($fontSizeTitle, 0, $fontPath, $title);
+        $titleWidth = abs($bboxTitle[2] - $bboxTitle[0]);
+        $titleHeight = abs($bboxTitle[7] - $bboxTitle[1]);
+
+        $bboxMain = imagettfbbox($fontSize, 0, $fontPath, $mainLine);
+        $mainWidth = abs($bboxMain[2] - $bboxMain[0]);
+        $mainHeight = abs($bboxMain[7] - $bboxMain[1]);
+
+        $bboxSecond = imagettfbbox($fontSize, 0, $fontPath, $secondLine);
+        $secondWidth = abs($bboxSecond[2] - $bboxSecond[0]);
+        $secondHeight = abs($bboxSecond[7] - $bboxSecond[1]);
+
+        $maxTextWidth = max($titleWidth, $mainWidth, $secondWidth);
 
         $generator = new BarcodeGeneratorPNG();
+        $barcodeData = $generator->getBarcode($item->internal_id, $generator::TYPE_CODE_128, 3, 60);
+        $barcodeImg = imagecreatefromstring($barcodeData);
+        $barcodeWidth = imagesx($barcodeImg);
+        $barcodeHeight = imagesy($barcodeImg);
 
-        $temp = tempnam(sys_get_temp_dir(), 'item_barcode');
+        $codeText = $item->internal_id;
+        $bboxCode = imagettfbbox($fontSizeCode, 0, $fontPath, $codeText);
+        $codeWidth = abs($bboxCode[2] - $bboxCode[0]);
+        $codeHeight = abs($bboxCode[7] - $bboxCode[1]);
 
-        file_put_contents($temp, $generator->getBarcode($item->internal_id, $generator::TYPE_CODE_128, 5, 70, $colour));
+        $currencySymbol = $item->currency_type ? $item->currency_type->symbol : 'S/';
+        $priceNumber = number_format($item->sale_unit_price, 2);
 
-        $headers = [
-            'Content-Type' => 'application/png',
-        ];
+        $bboxSymbol = imagettfbbox($fontSizePrice, 0, $fontPath, $currencySymbol);
+        $symbolWidth = abs($bboxSymbol[2] - $bboxSymbol[0]);
+        $symbolHeight = abs($bboxSymbol[7] - $bboxSymbol[1]);
 
-        return response()->download($temp, "{$item->internal_id}.png", $headers);
+        $bboxNumber = imagettfbbox($fontSizePrice, 0, $fontPath, $priceNumber);
+        $numberWidth = abs($bboxNumber[2] - $bboxNumber[0]);
+        $numberHeight = abs($bboxNumber[7] - $bboxNumber[1]);
 
+        $priceWidth = $symbolWidth + 8 + $numberWidth;
+        $priceHeight = max($symbolHeight, $numberHeight);
+
+        $finalWidth = max($barcodeWidth, $maxTextWidth, $codeWidth, $priceWidth) + $padding * 2;
+        $finalHeight = $padding + $titleHeight + $lineSpacing
+            + $mainHeight + $lineSpacing
+            + $secondHeight + $lineSpacing
+            + $barcodeHeight + $lineSpacing
+            + $codeHeight + $lineSpacing
+            + $priceHeight + $padding;
+
+        $image = imagecreatetruecolor($finalWidth, $finalHeight);
+        $white = imagecolorallocate($image, 255, 255, 255);
+        $black = imagecolorallocate($image, 0, 0, 0);
+        imagefill($image, 0, 0, $white);
+
+        $titleX = ($finalWidth - $titleWidth) / 2;
+        $y = $padding + $titleHeight;
+        for ($i = 0; $i <= 1; $i++) {
+            for ($j = 0; $j <= 1; $j++) {
+                imagettftext($image, $fontSizeTitle, 0, $titleX + $i, $padding + $titleHeight + $j, $black, $fontPath, $title);
+            }
+        }
+
+        $mainX = ($finalWidth - $mainWidth) / 2;
+        imagettftext($image, $fontSize, 0, $mainX, $y + $lineSpacing + $mainHeight, $black, $fontPath, $mainLine);
+        $y += $mainHeight + $lineSpacing * 2;
+
+        $secondX = ($finalWidth - $secondWidth) / 2;
+        imagettftext($image, $fontSize, 0, $secondX, $y + $secondHeight, $black, $fontPath, $secondLine);
+        $y += $secondHeight + $lineSpacing;
+
+        imagecopy(
+            $image,
+            $barcodeImg,
+            ($finalWidth - $barcodeWidth) / 2,
+            $y,
+            0,
+            0,
+            $barcodeWidth,
+            $barcodeHeight
+        );
+        $y += $barcodeHeight + $lineSpacing;
+
+        $codeX = ($finalWidth - $codeWidth) / 2;
+        imagettftext($image, $fontSizeCode, 0, $codeX, $y + $codeHeight, $black, $fontPath, $codeText);
+        $y += $codeHeight + $lineSpacing;
+
+        $priceX = ($finalWidth - $priceWidth) / 2;
+        for ($i = 0; $i < 2; $i++) {
+            imagettftext($image, $fontSizePrice, 0, $priceX + $i, $y + $priceHeight, $black, $fontPath, $currencySymbol);
+            imagettftext($image, $fontSizePrice, 0, $priceX + $symbolWidth + 8 + $i, $y + $priceHeight, $black, $fontPath, $priceNumber);
+        }
+
+        imagedestroy($barcodeImg);
+
+        return $image;
     }
 
 
