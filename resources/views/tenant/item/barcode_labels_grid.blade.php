753775673f851c714b633c6f142e90a1020eb10f
Andy <ndtrillo@gmail.com>
2025-10-29 16:55:55 -0500

    Issue | #335 | ajsuste de barcode

--- resources/views/tenant/item/barcode_labels_grid.blade.php
+++ resources/views/tenant/item/barcode_labels_grid.blade.php
@@ -24,7 +24,7 @@
         td.label-cell {
             width: {{ $width }}mm;
             max-width: {{ $width }}mm;
-            height: {{ $height - 18 }}mm;
+            height: {{ $height - 10 }}mm;
             max-height: {{ $height }}mm;
             padding: 0;
             margin: 0;
